d1c4b17e5f4cc2744eb078bf01833f10c01881b4
dbobrik2 <112850495+dbobrik2@users.noreply.github.com>
2023-01-31 12:27:08 +0100

    Changed.

--- main.py
+++ main.py
@@ -3,4 +3,16 @@ import random
 value_1 = random.randint(1, 6)
 value_2 = random.randint(1, 6)
 
-print("YOU GOT:",value_1, value_2)
+player_1 = value_1 + value_2
+
+value_3 = random.randint(1, 6)
+value_4 = random.randint(1, 6)
+
+player_2 =  value_3 + value_4
+
+if player_1 > player_2:
+    print("WIN 1 PLAYER!!!")
+elif player_1 < player_2:
+    print("WIN 2 PLAYER!!!")
+else:
+    print("DRAAAAAAW!!!")
